dc72532fffa71f515c84fdb17a1fc725f12932da
Joseph Acevedo <josephacevedo1997@gmail.com>
2021-12-02 21:35:58 -0600

    top half finished
    finished functions that calculate principal, interest, and total cost

--- js/site.js
+++ js/site.js
@@ -1,27 +1,84 @@
+
+// loanAmount = user input, how much the loan is for
+// numberOfPayments = user input, how many of payments will be made in terms of months
+// interestRate
+// monthlyPayment = displays how much the monthly payment will be
+// totalPrincipal = displays loan amount ("principal amount")
+// totalInterest = displays the total interest that will be accumulated 
+// totalCost = displays the total cost of the loan including interest
+// 
+// 
+
+
+
+
+
+
 function getValues(){
     // grab all user inputs
     let loanAmount = document.getElementById("loanAmount").value;
-    let payments = document.getElementById("payments").value;
-    let interest = document.getElementById("interest").value;
+    let numberOfPayments = document.getElementById("numberOfPayments").value;
+    let interestRate = document.getElementById("interestRate").value;
 
     
-    let monthly = monthlyPayments(loanAmount,payments,interest)
-     monthly = monthly.toFixed(2);
-    display(loanAmount,monthly);
+    let monthlyPaymentAmount = monthlyPayment(loanAmount,numberOfPayments,interestRate);
+     monthlyPaymentAmount = monthlyPaymentAmount.toFixed(2);
+
+     let totalCostAmount = totalCost(monthlyPaymentAmount,numberOfPayments); 
+     totalCostAmount = totalCostAmount.toFixed(2);
+
+    let totalInterestAmount = totalInterest(totalCostAmount,loanAmount);
+    totalInterestAmount = totalInterestAmount.toFixed(2);
+
+    display(monthlyPaymentAmount,loanAmount,totalInterestAmount,totalCostAmount);
+    
 
 }
 
-function monthlyPayments(loanAmount,payments,interest){
-   
-    let payment = loanAmount * (interest/1200)/(1 - Math.pow((1 + interest/1200), -payments));
-    return payment;
+function monthlyPayment(loanAmount,numberOfPayments,interestRate){
+//    formula calculates how much the monthly payments will be
+    let monthlyPaymentAmount = loanAmount * (interestRate/1200)/(1 - Math.pow((1 + interestRate/1200), - numberOfPayments));
+
+    return monthlyPaymentAmount;
 }
 
-function display(loanAmount,payment,c){
-    // shows total principal
- document.getElementById("totalPrincipal").innerHTML= `$ ${loanAmount}`;
- document.getElementById("monthlyPayment").innerHTML= `$ ${payment}`;
- 
+
+
+function totalInterest(totalCostAmount,totalPrincipal){
+    // gives total interest that will be payed 
+    let totalInterestAmount = totalCostAmount - totalPrincipal;
+    return totalInterestAmount;
+
+}
+
+
+
+function totalCost(monthlyPaymentAmount,numberOfPayments){
+    // calculates the total cost of the the loan
+    let totalCostAmount = monthlyPaymentAmount * numberOfPayments;
+    return totalCostAmount;
+}
+
+
+
+
+
+
+
+function display(monthlyPaymentAmount,loanAmount,totalInterestAmount,totalCostAmount){
+    // displays monthly payment
+    document.getElementById("monthlyPayment").innerHTML= `$ ${monthlyPaymentAmount}`;
+
+
+    // displays loan amount ("principal amount")
+    document.getElementById("totalPrincipal").innerHTML= `$ ${loanAmount}`;
+    
+    // displays the total amount of interest that will be payed
+    document.getElementById("totalInterest").innerHTML= `$ ${totalInterestAmount}`;
+
+
+// displays the total cost of the loan including interest
+    document.getElementById("totalCost").innerHTML=`$ ${totalCostAmount}`;
 
 }
 
